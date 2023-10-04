efc4379df7fad429a47e95fe28b3cc88f492aeea
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2023-10-04 22:41:02 +0300

    finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+let currentIndex = 0;
+
+function createGalleryItem(item, index) {
+    
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery-item');
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = item.original;
+
+    const galleryImg = document.createElement('img');
+    galleryImg.classList.add('gallery__image');
+    galleryImg.src = item.preview;
+    galleryImg.alt = item.description;
+    galleryImg.dataset.source = item.original;
+    galleryImg.dataset.index = index;
+
+    galleryLink.appendChild(galleryImg);
+    galleryItem.appendChild(galleryLink);
+
+    return galleryItem;
+}
+
+galleryItems.forEach((item) => {
+    const galleryElement = createGalleryItem(item);
+    galleryList.appendChild(galleryElement);
+})
+
+galleryList.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (event.target.classList.contains('gallery__image')) {
+        const fullSizeImg = event.target.dataset.source;
+        currentIndex = parseInt(event.target.dataset.index);
+
+        // alert('Клик на изображении с URL:' + fullSizeImg);
+
+        const instance = basicLightbox.create(`
+            <img src="${fullSizeImg}" width="800" height="600">
+        `);
+
+        instance.show();
+
+        document.addEventListener('keydown', function (event) {
+            if (event.key === 'Escape') {
+                instance.close();
+            } 
+        });
+    }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryList = document.querySelector('.gallery');
+    let currentIndex = 0;
 
-console.log(galleryItems);
+    function createGalleryItem(item, index) {
+    
+        const galleryItem = document.createElement('li');
+        galleryItem.classList.add('gallery__item');
+
+        const galleryLink = document.createElement('a');
+        galleryLink.classList.add('gallery__link');
+        galleryLink.href = item.original;
+
+        const galleryImg = document.createElement('img');
+        galleryImg.classList.add('gallery__image');
+        galleryImg.src = item.preview;
+        galleryImg.alt = item.description;
+        galleryImg.title = item.description;
+        galleryImg.dataset.source = item.original;
+        galleryImg.dataset.index = index;
+
+        galleryLink.appendChild(galleryImg);
+        galleryItem.appendChild(galleryLink);
+
+        return galleryItem;
+    }
+
+    galleryItems.forEach((item) => {
+        const galleryElement = createGalleryItem(item);
+        galleryList.appendChild(galleryElement);
+        console.log(item.description);
+    })
+
+    let lightbox = new SimpleLightbox('.gallery a', {captionDelay: 250});
+
+    lightbox.on('show.simplelightbox',() => {        
+    })
+})
